5ee2dcea154390a2415cbeb0e24e48867dfe0c7f
PauliCrespi <4377@holbertonschool.com>
2022-04-01 06:01:45 -0700

    strtok

--- strtok.c
+++ strtok.c
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-char* line()
-{
-	size_t sizebuff = 64;
-	char *buffer;
-=======
 #include "sshlib.h"
->>>>>>> 02e9323807f3c527360b5f71e84f1a8fc70fc616
-
 void tokenizer(char *line)
 {
 	char *tokenize;
@@ -18,14 +7,9 @@ void tokenizer(char *line)
 	if (tokenize == NULL)
 	{
 		printf("%s", tokenize);
-<<<<<<< HEAD
 		printf("No separators found");
-		return(1);
-=======
-		puts("No separators found");
 		exit(-1);
 		//return (1);
->>>>>>> 02e9323807f3c527360b5f71e84f1a8fc70fc616
 	}
 	while (tokenize)
 	{

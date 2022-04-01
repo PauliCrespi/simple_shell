a4f5247cefb432427b82b6bb25758bf9000ef1da
PauliCrespi <4377@holbertonschool.com>
2022-04-01 04:58:48 -0700

    tokenizer

---
+++ strtok.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+char* line()
+{
+	size_t sizebuff = 32;
+	char *buffer;
+
+	buffer = (char *)malloc(sizeof(char) * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("Unable to allocate buffer");
+		exit(1);
+	}
+	printf("$ ");
+	getline(&buffer, &sizebuff, stdin);
+	return(buffer);
+}
+int main()
+{
+	char *tokenize;
+	char *argument	= line();
+
+	tokenize = strtok(argument, " ");
+	if (tokenize == NULL)
+	{
+		printf("%s", tokenize);
+		puts("No separators found");
+		return(1);
+	}
+	while (tokenize)
+	{
+		printf("%s\n", tokenize);
+		tokenize = strtok(NULL, " ");
+	}
+	return (0);
+}

83617a42a8234b64a0604bb13cd3a0269362bb0c
Alex <asenges400@gmail.com>
2022-04-01 13:53:07 -0300

    Simple Shell - Alex

--- exec.c
+++ exec.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 /**
  * main - execve example
  *
@@ -9,30 +10,18 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
-	    char *argv[] = {"/bin/ls", "-l", "/tmp/", NULL};
-		int pid, status, i = 0;
+	char *argv[] = {"/bin/ls", "-l", "/tmp/", NULL};
+	int pid, status, i = 0;
 
-	        for(i = 0; i <= 4; i++)
+	for(i = 0; i <= 4; i++)
+	{
+		if ((pid = execve(argv[0], argv, NULL)) == -1)
 		{
-		    if ((pid = execve(argv[0], argv, NULL)) == -1)
-			        {
-					        perror("Error:");
-						    }
-		    while (wait(&status) != pid)
-		    {
-		    }
-		}	
-	    		return (0);
-=======
-    char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
-
-    printf("Before execve\n");
-    if (execve(argv[0], argv, NULL) == -1)
-    {
-        perror("Error:");
-    }
-    printf("After execve\n");
-    return (0);
->>>>>>> dfe7cd26f6bfcb0f68f14fea5199189cc15da5ef
+			perror("Error:");
+		}
+		while (wait(&status) != pid)
+		{
+		}
+	}	
+	return (0);
 }

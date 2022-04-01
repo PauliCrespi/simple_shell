3dad4bd69e0359421f49ebcc5cebc6d2c295cead
PauliCrespi <4377@holbertonschool.com>
2022-04-01 10:59:28 -0700

    Simple Shell - Alex & Paulina

--- wait.c
+++ wait.c
@@ -11,23 +11,31 @@
 int main(void)
 {
     pid_t child_pid;
-    int status;
-
-    child_pid = fork();
-    if (child_pid == -1)
-    {
-        perror("Error:");
-        return (1);
-    }
-    if (child_pid == 0)
-    {
-        printf("Wait for me, wait for me\n");
-        sleep(3);
-    }
-    else
-    {
-        wait(&status);
-        printf("Oh, it's all better now\n");
-    }
-    return (0);
+    int status, i = 0;
+    char *argv[] = {"/bin/ls", "-l", "/tmp/", NULL};
+	for (i = 0; i <= 4; i++)
+	{
+    		child_pid = fork();
+    		if (child_pid == -1)
+    		{
+        		perror("Error:");
+        		return (1);
+    		}
+    		if (child_pid == 0)
+    		{
+        	printf("Wait for me, wait for me\n");
+        	sleep(3);
+    		
+		if (execve(argv[0], argv, NULL) == -1)
+		{
+                        perror("Error:");
+		}
+		}
+    		else
+    		{
+        	wait(&status);
+        	printf("Oh, it's all better now\n");
+    		}
+	}
+    		return (0);
 }

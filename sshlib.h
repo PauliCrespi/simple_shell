788083f8916564f7e82b8da192496ddc42bac854
PauliCrespi <4377@holbertonschool.com>
2022-04-04 07:55:40 -0700

    ssh

--- sshlib.h
+++ sshlib.h
@@ -7,11 +7,11 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <unistd.h>
-
+int main(int ac, char **av);
 int prompt(void);
 //void ssh_strtok(char **arr, int size);
 char **tokenizer(char *line);
-int fwe(char *cmd, char *argv[]);
+int _fork(char **vtptr);
 #endif
 /* END OF FILE */
 

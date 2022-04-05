9649f2da54557b4d66e030bcff180b7bbcbca2a0
PauliCrespi <4377@holbertonschool.com>
2022-04-05 10:04:19 -0700

    strncmp

--- _strncmp.c
+++ _strncmp.c
@@ -9,7 +9,7 @@ int _strncmp(const char *s1, char *s2, int n)
 {
 	int i = 0;
 
-	while (i <= n)
+	while (i < n && s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] == s2[i])
 		{
@@ -20,16 +20,5 @@ int _strncmp(const char *s1, char *s2, int n)
 			return (-1);
 		}
 	}
-	if (s1[i] != '\0' && s2[i] == '\0')
-	{
-		return (-1);
-	}
-	else if (s1[i] == '\0' && s2[i] != '\0')
-	{
-		return (-1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }

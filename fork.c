cc17f78429179972cd369c7c7cbee6a5dd700bfd
PauliCrespi <4377@holbertonschool.com>
2022-04-08 11:01:16 -0700

    fork

--- fork.c
+++ fork.c
@@ -6,28 +6,25 @@
  */
 pid_t  _fork(char **ptrbuf)
 {
-	int status;
+	int status = 0;
 	pid_t pid = -1;
 
 	pid = fork();
 	if (pid == -1)
 	{
-		perror("Error: ");
-		return (-1);
+		perror("hsh error");
 	}
 	if (pid == 0)
 	{
-		sleep(1);
 		if (execve(ptrbuf[0], ptrbuf, NULL) == -1)
 		{
-			perror("Error");
-			return (-1);
+		        perror("hsh error");
 		}
 	}
 	else
 	{
-		wait(&status);
-		printf("Status: %d\n", status);
+	        wait(&status);
+	        printf("hsh status: %d\n", status);
 	}
 	return (pid);
 }

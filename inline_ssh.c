4a9dfceb0dba8ee962dc2f3551fa1bd8f856bd68
PauliCrespi <4377@holbertonschool.com>
2022-04-11 04:50:45 -0700

    betty inline

--- inline_ssh.c
+++ inline_ssh.c
@@ -14,6 +14,7 @@ int inline_ssh(int ac __attribute__((unused)), char **argv)
 	path_cpy = malloc(_strlen(_getenv("PATH")) * sizeof(char));
 	_strcpy(path_cpy, _getenv("PATH"));
 	path_tokens = _tokenize(tokenizer(path_cpy, "="), ":");
+	free(path_cpy);
 	clean_buffer(buf);
 	cpy_argv_to_buf(buf, argv, 1);
 	if (manage_builtins(buf[0]) == 0)
@@ -35,7 +36,6 @@ int inline_ssh(int ac __attribute__((unused)), char **argv)
 		printf("ssh: command not found.\n");
 		exit(0);
 	}
-	free(path_cpy);
 	free(path_tokens);
 	return (0);
 }

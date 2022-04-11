6443f3cc36df18065d8fbb4de595e7ade0ac6f0a
PauliCrespi <4377@holbertonschool.com>
2022-04-10 21:41:14 -0700

    betty prompt

--- prompt.c
+++ prompt.c
@@ -1,25 +1,48 @@
 #include "sshlib.h"
-
+/**
+ *prompt - interactive mode function
+ *Return: 0 if success
+ */
 int prompt(void)
 {
-        size_t sizebuff = 64;
-	char *buffer = NULL;
-        char **path_tokens, **vtline;
-        int response = 0;
+	size_t sizebuff = 64;
+	char *buf[1024] = { NULL };
+	char *buffer = NULL, *path_cpy = NULL, **path_tokens = NULL, **vtline = NULL;
+	int response = 0, flag = 0;
 
-	path_tokens = _tokenize(tokenizer(_getenv("PATH"), "="), ":");	
-        while(response != -1)
-        {
+	path_cpy = malloc(_strlen(_getenv("PATH")) * sizeof(char));
+	_strcpy(path_cpy, _getenv("PATH"));
+	path_tokens = _tokenize(tokenizer(path_cpy, "="), ":");
+	while (response != -1)
+	{
 		printf("[hshc]>_ ");
 		response = getline(&buffer, &sizebuff, stdin);
 		buffer[_strlen(buffer) - 1] = '\0';
 		vtline = tokenizer(buffer, " ");
-		// Manage Exit // 
-		__exit(vtline);
-		_tokexe(vtline, path_tokens);	
+		clean_buffer(buf);
+		if (manage_builtins(vtline[0]) == 0)
+		{
+			continue;
+		}
+		cpy_argv_to_buf(buf, vtline, 0);
+		flag = is_combined_cmd(vtline);
+		if (flag == -1)
+			flag = look_cmd_in_path(vtline, path_tokens, buf, 0);
+		else
+			printf("hsh: command not found.\n");
+		if (flag == -1)
+		{
+			printf("hsh: command not found.\n");
+			continue;
+		}
+		if (flag == 0)
+			_fork(buf);
+		else
+			printf("hsh: command not found.\n");
 		free(vtline);
 		free(buffer);
-		free(path_tokens);
 	}
+	free(path_cpy);
+	free(path_tokens);
 	return (response);
 }

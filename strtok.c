b2f3ff0843a90574f1cf8568b7e3a03c9a394f36
PauliCrespi <4377@holbertonschool.com>
2022-04-10 21:46:27 -0700

    betty done

--- strtok.c
+++ strtok.c
@@ -1,40 +1,44 @@
 #include "sshlib.h"
-
+/**
+ *tokenizer - tokenize function
+ *@line : command line
+ *@sep : separator character
+ *Return: tokenized
+ */
 char **tokenizer(char *line, const char *sep)
 {
-    char **tokens = malloc(sizeof(char *));
-    *tokens = NULL;
-    size_t n = 1;
-    int success;
+	char **tmp = NULL, **tokens = malloc(sizeof(char *));
+	size_t n = 1;
+	int success = 0;
+	char *p = NULL;
 
-    char *p = strtok(line, sep);
-    if (p != NULL)
-        success = 1;
-    else
-        success = 0;
-
-    while (success)
-    {
-        char **tmp = realloc(tokens,(n + 1) * sizeof(char *));
-        if (tmp == NULL)
-        {
-            free(tokens);
-            tokens = NULL;
-            success = 0;
-        }
-        else
-        {
-            tokens = tmp;
-            tokens[n - 1] = p;
-            tokens[n] = NULL;
-            ++n;
-            p = strtok(NULL, sep);
-            if (p != NULL)
-                success = 1;
-            else
-                success = 0;
-
-        }
-    }
-    return (tokens);
+	p = strtok(line, sep);
+	if (p != NULL)
+		success = 1;
+	else
+		success = 0;
+	while (success)
+	{
+		tmp = realloc(tokens, (n + 1) * sizeof(char *));
+		if (tmp == NULL)
+		{
+			free(tokens);
+			tokens = NULL;
+			success = 0;
+		}
+		else
+		{
+			tokens = tmp;
+			tokens[n - 1] = p;
+			tokens[n] = NULL;
+			++n;
+			p = strtok(NULL, sep);
+			if (p != NULL)
+				success = 1;
+			else
+				success = 0;
+		}
+	}
+	free(p);
+	return (tokens);
 }

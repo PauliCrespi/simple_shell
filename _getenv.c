ad7abccfde0594435dcf6d0c1d1b33b914ee6576
PauliCrespi <4377@holbertonschool.com>
2022-04-11 04:06:31 -0700

    betty done

--- _getenv.c
+++ _getenv.c
@@ -1,29 +1,32 @@
 #include "sshlib.h"
-
+/**
+ *_getenv - get enviroment variable function
+ *@name : PATH
+ *Return: PATH or NULL
+ */
 char *_getenv(const char *name)
 {
-	extern char **environ;
-	int i = 0; 
+	int i = 0;
 
-	while (environ[i] != NULL)                                                      
+	while (environ[i] != NULL)
 	{
-	    if (_strncmp(name, environ[i], _strlen(name)) == 0)
-	    {
-	        return (environ[i]);
-	    }                                                 
-		i++;                                                                    
-	}	
+		if (_strncmp(name, environ[i], _strlen(name)) == 0)
+		{
+			return (environ[i]);
+		}
+		i++;
+	}
 	return (NULL);
 }
-void _getenv_all()
+/**
+ *_getenv_all - func
+ */
+void _getenv_all(void)
 {
-	extern char **environ;
-	int i; 
+	int i = 0;
 
-	i = 0;
-	while (environ[i] != NULL)                                                      
+	for (i = 0; environ[i] != NULL; i++)
 	{
-	    printf("%s\n", environ[i]);                                                 
-		i++;                                                                    
+		printf("%s\n", environ[i]);
 	}
 }

29d5e7d8bcada38948d12f60ae8c0a6e34469fd1
PauliCrespi <4377@holbertonschool.com>
2022-04-11 10:18:54 -0700

    inline

--- inline_ssh.c
+++ inline_ssh.c
@@ -5,37 +5,45 @@
  *@argv : command line
  *Return: Shell Status.
  */
-int inline_ssh(int ac __attribute__((unused)), char **argv)
+int inline_ssh(int ac, char **argv)
 {
-	int flag = 0;
-	char *buf[1024], *path_cpy = NULL;
-	char **path_tokens;
+	int flag = 0, i = 1, j = 0;
+	char *path_cpy, *path = NULL;
+	char **vtline = NULL;
+	char **path_tok = NULL, **path_tokens = NULL, **ptrbuf = NULL;
 
-	path_cpy = malloc(_strlen(_getenv("PATH")) * sizeof(char));
-	_strcpy(path_cpy, _getenv("PATH"));
-	path_tokens = _tokenize(tokenizer(path_cpy, "="), ":");
+	path = _getenv("PATH");
+	path_cpy = malloc(_strlen(path) * sizeof(char));
+	_strcpy(path_cpy, path); 
+	path_tok = tokenizer(path_cpy, "=");                
+	path_tokens = tokenizer(path_tok[1], ":");	
 	free(path_cpy);
-	clean_buffer(buf);
-	cpy_argv_to_buf(buf, argv, 1);
-	if (manage_builtins(buf[0]) == 0)
+	free(path_tok);
+	vtline = malloc(ac * sizeof(char *));
+	for (i = 1; argv[i] != NULL; i++, j++)
+	{
+		vtline[j] = malloc(_strlen(argv[i]) * sizeof(char));
+		_strcpy(vtline[j], argv[i]);
+	}
+	if (manage_builtins(vtline[0]) == 0)
 	{
 		exit(0);
 	}
-	flag = is_combined_cmd(buf);
+	flag = is_combined_cmd(vtline);
 	if (flag == -1)
-		flag = look_cmd_in_path(argv, path_tokens, buf, 1);
+		ptrbuf = look_cmd_in_path(vtline, path_tokens, 0);
 	else
 		printf("hsh: command not found.\n");
-	if (flag == 0)
+	if (ptrbuf != NULL)
 	{
-		_fork(buf);
-
+		_fork(ptrbuf);
 	}
 	else
 	{
 		printf("ssh: command not found.\n");
 		exit(0);
 	}
+	free(vtline);
 	free(path_tokens);
 	return (0);
 }

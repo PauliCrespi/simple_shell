83c59dce529636fc8cc833aadc8eeda5385fd372
Alex <asenges400@gmail.com>
2022-04-12 20:45:48 -0300

    Update Apr12

--- env-main.c
+++ env-main.c
@@ -7,18 +7,11 @@
  */
 int main(int ac, char **av)
 {
-	int interactive_ssh = 1;
-	int noninteractive_ssh = 1;
+	int hsh_resp = 1;
 
 	if (ac == 1)
-	{
-		interactive_ssh = prompt();
-		printf("ssh response: %d\n", interactive_ssh);
-	}
+		hsh_resp = prompt();
 	else
-	{
-		noninteractive_ssh = inline_ssh(ac, av);
-		printf("ssh response: %d\n", noninteractive_ssh);
-	}
-	return (0);
+		hsh_resp = inline_ssh(ac, av);
+	return (hsh_resp);
 }
